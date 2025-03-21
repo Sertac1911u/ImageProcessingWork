a650c395ec3941159f2888b94c0a5daa5f364b70
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-03-21 18:09:53 +0300

    Add project files.

---
+++ ImageProcessingWork/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace ImageProcessingWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.selectImageButton = new System.Windows.Forms.Button();
+            this.uzaysalFiltre_Btn = new System.Windows.Forms.Button();
+            this.yuksekFiltre_Btn = new System.Windows.Forms.Button();
+            this.clr_Button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(610, 411);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(637, 54);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(610, 411);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // selectImageButton
+            // 
+            this.selectImageButton.Location = new System.Drawing.Point(134, 718);
+            this.selectImageButton.Name = "selectImageButton";
+            this.selectImageButton.Size = new System.Drawing.Size(185, 59);
+            this.selectImageButton.TabIndex = 2;
+            this.selectImageButton.Text = "Fotoğraf Seç";
+            this.selectImageButton.UseVisualStyleBackColor = true;
+            this.selectImageButton.Click += new System.EventHandler(this.selectImageButton_Click);
+            // 
+            // uzaysalFiltre_Btn
+            // 
+            this.uzaysalFiltre_Btn.Location = new System.Drawing.Point(389, 718);
+            this.uzaysalFiltre_Btn.Name = "uzaysalFiltre_Btn";
+            this.uzaysalFiltre_Btn.Size = new System.Drawing.Size(185, 59);
+            this.uzaysalFiltre_Btn.TabIndex = 3;
+            this.uzaysalFiltre_Btn.Text = "Ortalama Filtreleme";
+            this.uzaysalFiltre_Btn.UseVisualStyleBackColor = true;
+            this.uzaysalFiltre_Btn.Click += new System.EventHandler(this.uzaysalFiltre_Btn_Click);
+            // 
+            // yuksekFiltre_Btn
+            // 
+            this.yuksekFiltre_Btn.Location = new System.Drawing.Point(648, 718);
+            this.yuksekFiltre_Btn.Name = "yuksekFiltre_Btn";
+            this.yuksekFiltre_Btn.Size = new System.Drawing.Size(185, 59);
+            this.yuksekFiltre_Btn.TabIndex = 4;
+            this.yuksekFiltre_Btn.Text = "Yüksek Geçiren\n";
+            this.yuksekFiltre_Btn.UseVisualStyleBackColor = true;
+            this.yuksekFiltre_Btn.Click += new System.EventHandler(this.yuksekFiltre_Btn_Click);
+            // 
+            // clr_Button
+            // 
+            this.clr_Button.Location = new System.Drawing.Point(918, 718);
+            this.clr_Button.Name = "clr_Button";
+            this.clr_Button.Size = new System.Drawing.Size(185, 59);
+            this.clr_Button.TabIndex = 5;
+            this.clr_Button.Text = "Clear";
+            this.clr_Button.UseVisualStyleBackColor = true;
+            this.clr_Button.Click += new System.EventHandler(this.clr_Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1272, 800);
+            this.Controls.Add(this.clr_Button);
+            this.Controls.Add(this.yuksekFiltre_Btn);
+            this.Controls.Add(this.uzaysalFiltre_Btn);
+            this.Controls.Add(this.selectImageButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button selectImageButton;
+        private System.Windows.Forms.Button uzaysalFiltre_Btn;
+        private System.Windows.Forms.Button yuksekFiltre_Btn;
+        private System.Windows.Forms.Button clr_Button;
+    }
+}
+
---
+++ ImageProcessingWork/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageProcessingWork
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap originalImage, processingImage;
+        //Select image buton
+        private void selectImageButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Image Files(*.jpg;*.png;*.bmp)|*.jpg;*.png;*.bmp";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(open.FileName);
+                pictureBox1.Image = originalImage;
+            }
+        }
+        //Uzaysal fonksiyon ortalama
+        private Bitmap OrtalamaFiltre(Bitmap original)
+        {
+            Bitmap processedImage = new Bitmap(original.Width, original.Height);
+
+            int kernelSize = 3;
+            int kernelSum = 9;
+
+            for (int x = 1; x < original.Width - 1; x++)
+            {
+                for (int y = 1; y < original.Height - 1; y++)
+                {
+                    int summR = 0, summG = 0, summB = 0;
+
+                    for (int kx = -1; kx <= 1; kx++)
+                    {
+                        for (int ky = -1; ky <= 1; ky++)
+                        {
+                            Color komsuPixel = original.GetPixel(x + kx, y + ky);
+                            summR += komsuPixel.R;
+                            summG += komsuPixel.G;
+                            summB += komsuPixel.B;
+                        }
+                    }
+
+                    Color newColor = Color.FromArgb(summR / kernelSum, summG / kernelSum, summB / kernelSum);
+                    processedImage.SetPixel(x, y, newColor);
+                }
+            }
+
+            return processedImage;
+        }
+        //Uzaysal filtreleme buton
+        private void uzaysalFiltre_Btn_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+                return;
+
+            processingImage = OrtalamaFiltre(originalImage);
+            pictureBox2.Image = processingImage;
+        }
+        //frekans fonksiyon
+        private Bitmap YuksekGecirenFiltre(Bitmap original)
+        {
+            Bitmap yeniResim = new Bitmap(original.Width, original.Height);
+
+            int[,] kernel = {
+                { -1, -1, -1 },
+                { -1,  9, -1 },
+                { -1, -1, -1 }
+                };
+
+            for (int x = 1; x < original.Width - 1; x++)
+            {
+                for (int y = 1; y < original.Height - 1; y++)
+                {
+                    int toplamR = 0, toplamG = 0, toplamB = 0;
+
+                    for (int kx = -1; kx <= 1; kx++)
+                    {
+                        for (int ky = -1; ky <= 1; ky++)
+                        {
+                            Color komsuPixel = original.GetPixel(x + kx, y + ky);
+                            int kernelDeger = kernel[kx + 1, ky + 1];
+
+                            toplamR += komsuPixel.R * kernelDeger;
+                            toplamG += komsuPixel.G * kernelDeger;
+                            toplamB += komsuPixel.B * kernelDeger;
+                        }
+                    }
+
+                    toplamR = Math.Min(Math.Max(toplamR, 0), 255);
+                    toplamG = Math.Min(Math.Max(toplamG, 0), 255);
+                    toplamB = Math.Min(Math.Max(toplamB, 0), 255);
+
+                    yeniResim.SetPixel(x, y, Color.FromArgb(toplamR, toplamG, toplamB));
+                }
+            }
+            return yeniResim;
+        }
+
+        //frekans filtreleme btuon
+        private void yuksekFiltre_Btn_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+                return;
+
+            processingImage = YuksekGecirenFiltre(originalImage);
+            pictureBox2.Image = processingImage;
+        }
+
+        private void clr_Button_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox2.Image = null;
+            originalImage = null;
+            processingImage = null;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
