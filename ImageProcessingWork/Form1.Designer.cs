21afc373eebf3466db8b80c406ab9640887cae0f
Sertac1911u <114613689+Sertac1911u@users.noreply.github.com>
2025-03-22 18:25:11 +0300

    updated

--- ImageProcessingWork/Form1.Designer.cs
+++ ImageProcessingWork/Form1.Designer.cs
@@ -31,89 +31,109 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.selectImageButton = new System.Windows.Forms.Button();
-            this.uzaysalFiltre_Btn = new System.Windows.Forms.Button();
-            this.yuksekFiltre_Btn = new System.Windows.Forms.Button();
+            this.apply_btn = new System.Windows.Forms.Button();
             this.clr_Button = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(48, 26);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(610, 411);
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(637, 54);
+            this.pictureBox2.Location = new System.Drawing.Point(402, 26);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(610, 411);
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // selectImageButton
             // 
-            this.selectImageButton.Location = new System.Drawing.Point(134, 718);
+            this.selectImageButton.Location = new System.Drawing.Point(158, 391);
+            this.selectImageButton.Margin = new System.Windows.Forms.Padding(2);
             this.selectImageButton.Name = "selectImageButton";
-            this.selectImageButton.Size = new System.Drawing.Size(185, 59);
+            this.selectImageButton.Size = new System.Drawing.Size(175, 48);
             this.selectImageButton.TabIndex = 2;
             this.selectImageButton.Text = "Fotoğraf Seç";
             this.selectImageButton.UseVisualStyleBackColor = true;
             this.selectImageButton.Click += new System.EventHandler(this.selectImageButton_Click);
             // 
-            // uzaysalFiltre_Btn
+            // apply_btn
             // 
-            this.uzaysalFiltre_Btn.Location = new System.Drawing.Point(389, 718);
-            this.uzaysalFiltre_Btn.Name = "uzaysalFiltre_Btn";
-            this.uzaysalFiltre_Btn.Size = new System.Drawing.Size(185, 59);
-            this.uzaysalFiltre_Btn.TabIndex = 3;
-            this.uzaysalFiltre_Btn.Text = "Ortalama Filtreleme";
-            this.uzaysalFiltre_Btn.UseVisualStyleBackColor = true;
-            this.uzaysalFiltre_Btn.Click += new System.EventHandler(this.uzaysalFiltre_Btn_Click);
-            // 
-            // yuksekFiltre_Btn
-            // 
-            this.yuksekFiltre_Btn.Location = new System.Drawing.Point(648, 718);
-            this.yuksekFiltre_Btn.Name = "yuksekFiltre_Btn";
-            this.yuksekFiltre_Btn.Size = new System.Drawing.Size(185, 59);
-            this.yuksekFiltre_Btn.TabIndex = 4;
-            this.yuksekFiltre_Btn.Text = "Yüksek Geçiren\n";
-            this.yuksekFiltre_Btn.UseVisualStyleBackColor = true;
-            this.yuksekFiltre_Btn.Click += new System.EventHandler(this.yuksekFiltre_Btn_Click);
+            this.apply_btn.Location = new System.Drawing.Point(338, 391);
+            this.apply_btn.Margin = new System.Windows.Forms.Padding(2);
+            this.apply_btn.Name = "apply_btn";
+            this.apply_btn.Size = new System.Drawing.Size(153, 48);
+            this.apply_btn.TabIndex = 3;
+            this.apply_btn.Text = "Uygula";
+            this.apply_btn.UseVisualStyleBackColor = true;
+            this.apply_btn.Click += new System.EventHandler(this.apply_btn_Click);
             // 
             // clr_Button
             // 
-            this.clr_Button.Location = new System.Drawing.Point(918, 718);
+            this.clr_Button.Location = new System.Drawing.Point(495, 391);
+            this.clr_Button.Margin = new System.Windows.Forms.Padding(2);
             this.clr_Button.Name = "clr_Button";
-            this.clr_Button.Size = new System.Drawing.Size(185, 59);
+            this.clr_Button.Size = new System.Drawing.Size(71, 48);
             this.clr_Button.TabIndex = 5;
             this.clr_Button.Text = "Clear";
             this.clr_Button.UseVisualStyleBackColor = true;
             this.clr_Button.Click += new System.EventHandler(this.clr_Button_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(350, 352);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(212, 21);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(130, 352);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 18);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Filtreleme Algoritması Seç :";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1272, 800);
+            this.ClientSize = new System.Drawing.Size(768, 458);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.clr_Button);
-            this.Controls.Add(this.yuksekFiltre_Btn);
-            this.Controls.Add(this.uzaysalFiltre_Btn);
+            this.Controls.Add(this.apply_btn);
             this.Controls.Add(this.selectImageButton);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -122,9 +142,10 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button selectImageButton;
-        private System.Windows.Forms.Button uzaysalFiltre_Btn;
-        private System.Windows.Forms.Button yuksekFiltre_Btn;
+        private System.Windows.Forms.Button apply_btn;
         private System.Windows.Forms.Button clr_Button;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ImageProcessingWork/Form1.cs
+++ ImageProcessingWork/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -7,7 +8,7 @@ namespace ImageProcessingWork
     public partial class Form1 : Form
     {
         public Bitmap originalImage, processingImage;
-        //Select image buton
+        //Select image butonu
         private void selectImageButton_Click(object sender, EventArgs e)
         {
             OpenFileDialog open = new OpenFileDialog();
@@ -19,95 +20,414 @@ namespace ImageProcessingWork
                 pictureBox1.Image = originalImage;
             }
         }
-        //Uzaysal fonksiyon ortalama
-        private Bitmap OrtalamaFiltre(Bitmap original)
+
+
+        // Ortalama (Mean) Filtresi
+        private Bitmap MeanFilter(Bitmap image)
         {
-            Bitmap processedImage = new Bitmap(original.Width, original.Height);
-
-            int kernelSize = 3;
-            int kernelSum = 9;
-
-            for (int x = 1; x < original.Width - 1; x++)
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+            for (int x = 1; x < image.Width - 1; x++)
             {
-                for (int y = 1; y < original.Height - 1; y++)
+                for (int y = 1; y < image.Height - 1; y++)
                 {
-                    int summR = 0, summG = 0, summB = 0;
-
-                    for (int kx = -1; kx <= 1; kx++)
+                    int avgR = 0, avgG = 0, avgB = 0;
+                    for (int i = -1; i <= 1; i++)
                     {
-                        for (int ky = -1; ky <= 1; ky++)
+                        for (int j = -1; j <= 1; j++)
                         {
-                            Color komsuPixel = original.GetPixel(x + kx, y + ky);
-                            summR += komsuPixel.R;
-                            summG += komsuPixel.G;
-                            summB += komsuPixel.B;
+                            Color pixel = image.GetPixel(x + i, y + j);
+                            avgR += pixel.R;
+                            avgG += pixel.G;
+                            avgB += pixel.B;
                         }
                     }
-
-                    Color newColor = Color.FromArgb(summR / kernelSum, summG / kernelSum, summB / kernelSum);
-                    processedImage.SetPixel(x, y, newColor);
+                    avgR /= 9; avgG /= 9; avgB /= 9;
+                    filteredImage.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
                 }
             }
-
-            return processedImage;
+            return filteredImage;
         }
-        //Uzaysal filtreleme buton
-        private void uzaysalFiltre_Btn_Click(object sender, EventArgs e)
-        {
-            if (originalImage == null)
-                return;
 
-            processingImage = OrtalamaFiltre(originalImage);
-            pictureBox2.Image = processingImage;
+        // Gaussian Blurring Filtresi
+        private Bitmap GaussianBlur(Bitmap image)
+        {
+            double[,] kernel = {
+        { 1, 2, 1 },
+        { 2, 4, 2 },
+        { 1, 2, 1 }};
+
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    double sumR = 0, sumG = 0, sumB = 0;
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color pixel = image.GetPixel(x + i, y + j);
+                            sumR += pixel.R * kernel[i + 1, j + 1];
+                            sumG += pixel.G * kernel[i + 1, j + 1];
+                            sumB += pixel.B * kernel[i + 1, j + 1];
+                        }
+                    }
+                    sumR /= 16; sumG /= 16; sumB /= 16;
+                    filteredImage.SetPixel(x, y, Color.FromArgb((int)sumR, (int)sumG, (int)sumB));
+                }
+            }
+            return filteredImage;
         }
-        //frekans fonksiyon
-        private Bitmap YuksekGecirenFiltre(Bitmap original)
-        {
-            Bitmap yeniResim = new Bitmap(original.Width, original.Height);
 
+        // Laplace Filtresi
+        private Bitmap LaplaceFilter(Bitmap image)
+        {
             int[,] kernel = {
-                { -1, -1, -1 },
-                { -1,  9, -1 },
-                { -1, -1, -1 }
-                };
+        { 0,  1, 0 },
+        { 1, -4, 1 },
+        { 0,  1, 0 }};
 
-            for (int x = 1; x < original.Width - 1; x++)
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
             {
-                for (int y = 1; y < original.Height - 1; y++)
+                for (int y = 1; y < image.Height - 1; y++)
                 {
-                    int toplamR = 0, toplamG = 0, toplamB = 0;
+                    int sum = 0;
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
+                            sum += image.GetPixel(x + i, y + j).R * kernel[i + 1, j + 1];
 
-                    for (int kx = -1; kx <= 1; kx++)
-                    {
-                        for (int ky = -1; ky <= 1; ky++)
+                    sum = Math.Min(Math.Max(sum, 0), 255);
+                    filteredImage.SetPixel(x, y, Color.FromArgb(sum, sum, sum));
+                }
+            }
+            return filteredImage;
+        }
+
+        // Sobel Filtresi
+        private Bitmap SobelFilter(Bitmap image)
+        {
+            int[,] gx = { { -1, 0, 1 }, { -2, 0, 2 }, { -1, 0, 1 } };
+            int[,] gy = { { 1, 2, 1 }, { 0, 0, 0 }, { -1, -2, -1 } };
+
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int sumX = 0, sumY = 0;
+                    for (int i = -1; i <= 1; i++)
+                        for (int j = -1; j <= 1; j++)
                         {
-                            Color komsuPixel = original.GetPixel(x + kx, y + ky);
-                            int kernelDeger = kernel[kx + 1, ky + 1];
+                            int pixelVal = image.GetPixel(x + i, y + j).R;
+                            sumX += gx[i + 1, j + 1] * pixelVal;
+                            sumY += gy[i + 1, j + 1] * pixelVal;
+                        }
 
-                            toplamR += komsuPixel.R * kernelDeger;
-                            toplamG += komsuPixel.G * kernelDeger;
-                            toplamB += komsuPixel.B * kernelDeger;
+                    int sum = (int)Math.Sqrt(sumX * sumX + sumY * sumY);
+                    sum = Math.Min(Math.Max(sum, 0), 255);
+                    filteredImage.SetPixel(x, y, Color.FromArgb(sum, sum, sum));
+                }
+            }
+            return filteredImage;
+        }
+        // Prewitt Filtresi
+        private Bitmap PrewittFilter(Bitmap image)
+        {
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            int[,] Gx = {{-1, 0, 1},
+                  {-1, 0, 1},
+                  {-1, 0, 1}};
+
+            int[,] Gy = {{-1, -1, -1},
+                  { 0,  0,  0},
+                  { 1,  1,  1}};
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int sumX = 0;
+                    int sumY = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            int gray = image.GetPixel(x + i, y + j).R;
+
+                            sumX += gray * Gx[i + 1, j + 1];
+                            sumY += gray * Gy[i + 1, j + 1];
                         }
                     }
 
-                    toplamR = Math.Min(Math.Max(toplamR, 0), 255);
-                    toplamG = Math.Min(Math.Max(toplamG, 0), 255);
-                    toplamB = Math.Min(Math.Max(toplamB, 0), 255);
+                    int val = (int)Math.Sqrt(sumX * sumX + sumY * sumY);
+                    val = Math.Max(0, Math.Min(255, val));
 
-                    yeniResim.SetPixel(x, y, Color.FromArgb(toplamR, toplamG, toplamB));
+                    filteredImage.SetPixel(x, y, Color.FromArgb(val, val, val));
                 }
             }
-            return yeniResim;
+
+            return filteredImage;
+        }
+        //Minimum Filtre
+        private Bitmap MinFilter(Bitmap image)
+        {
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int minR = 255, minG = 255, minB = 255;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color c = image.GetPixel(x + i, y + j);
+                            if (c.R < minR) minR = c.R;
+                            if (c.G < minG) minG = c.G;
+                            if (c.B < minB) minB = c.B;
+                        }
+                    }
+
+                    filteredImage.SetPixel(x, y, Color.FromArgb(minR, minG, minB));
+                }
+            }
+            return filteredImage;
+        }
+        //Maksimum Filtre
+        private Bitmap MaxFilter(Bitmap image)
+        {
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    int maxR = 0, maxG = 0, maxB = 0;
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color c = image.GetPixel(x + i, y + j);
+                            if (c.R > maxR) maxR = c.R;
+                            if (c.G > maxG) maxG = c.G;
+                            if (c.B > maxB) maxB = c.B;
+                        }
+                    }
+
+                    filteredImage.SetPixel(x, y, Color.FromArgb(maxR, maxG, maxB));
+                }
+            }
+            return filteredImage;
+        }
+        //median Filtre
+        private Bitmap MedianFilter(Bitmap image)
+        {
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    List<int> reds = new List<int>();
+                    List<int> greens = new List<int>();
+                    List<int> blues = new List<int>();
+
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color c = image.GetPixel(x + i, y + j);
+                            reds.Add(c.R);
+                            greens.Add(c.G);
+                            blues.Add(c.B);
+                        }
+                    }
+
+                    reds.Sort();
+                    greens.Sort();
+                    blues.Sort();
+
+                    int medianR = reds[4];
+                    int medianG = greens[4];
+                    int medianB = blues[4];
+
+                    filteredImage.SetPixel(x, y, Color.FromArgb(medianR, medianG, medianB));
+                }
+            }
+
+            return filteredImage;
+        }
+        //K-en Yakın Komşu Filtre
+        private Bitmap KNearestFilter(Bitmap image, int k = 5)
+        {
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 1; x < image.Width - 1; x++)
+            {
+                for (int y = 1; y < image.Height - 1; y++)
+                {
+                    Color center = image.GetPixel(x, y);
+                    int centerR = center.R;
+                    int centerG = center.G;
+                    int centerB = center.B;
+
+                    List<(double distance, Color pixelColor)> distances = new List<(double, Color)>();
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            Color c = image.GetPixel(x + i, y + j);
+
+                            double d = (c.R - centerR) * (c.R - centerR) +
+                                       (c.G - centerG) * (c.G - centerG) +
+                                       (c.B - centerB) * (c.B - centerB);
+
+                            distances.Add((d, c));
+                        }
+                    }
+
+                    distances.Sort((a, b) => a.distance.CompareTo(b.distance));
+
+                    int sumR = 0, sumG = 0, sumB = 0;
+                    for (int n = 0; n < k; n++)
+                    {
+                        sumR += distances[n].pixelColor.R;
+                        sumG += distances[n].pixelColor.G;
+                        sumB += distances[n].pixelColor.B;
+                    }
+
+                    int avgR = sumR / k;
+                    int avgG = sumG / k;
+                    int avgB = sumB / k;
+
+                    filteredImage.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                }
+            }
+
+            return filteredImage;
+        }
+        // Basit Alçak Geçiren Filtre
+        private Bitmap LowPassFilter(Bitmap image)
+        {
+            return MeanFilter(image); 
         }
 
-        //frekans filtreleme btuonu
-        private void yuksekFiltre_Btn_Click(object sender, EventArgs e)
+        // Basit Yüksek Geçiren Filtre
+        private Bitmap HighPassFilter(Bitmap image)
+        {
+            Bitmap blurred = MeanFilter(image);
+            Bitmap filteredImage = new Bitmap(image.Width, image.Height);
+
+            for (int x = 0; x < image.Width; x++)
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color origPixel = image.GetPixel(x, y);
+                    Color blurPixel = blurred.GetPixel(x, y);
+
+                    int r = Math.Min(Math.Max(origPixel.R - blurPixel.R + 128, 0), 255);
+                    int g = Math.Min(Math.Max(origPixel.G - blurPixel.G + 128, 0), 255);
+                    int b = Math.Min(Math.Max(origPixel.B - blurPixel.B + 128, 0), 255);
+
+                    filteredImage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+
+            return filteredImage;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.AddRange(new String[]
+            {
+                "Ortalama (Mean) Filtresi",
+                "Gaussian Blurring Filtre",
+                "Laplace Filtresi",
+                "Sobel Filtresi",
+                "Alçak Geçiren Filtre",
+                "Yüksek Geçiren Filtre",
+                "Prewitt Filtresi",
+                "Minimum Filtre",
+                "Maximum Filtre",
+                "Median Filtre",
+                "K-en Yakın Komşu Filtre"
+            });
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void apply_btn_Click(object sender, EventArgs e)
         {
             if (originalImage == null)
+            {
+                MessageBox.Show("Önce bir görüntü seçin!");
                 return;
+            }
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    // Ortalama (Mean) Filtresi
+                    processingImage = MeanFilter(originalImage);
+                    break;
+                case 1:
+                    // Gaussian Blurring
+                    processingImage = GaussianBlur(originalImage);
+                    break;
+                case 2:
+                    // Laplace Filtresi
+                    processingImage = LaplaceFilter(originalImage);
+                    break;
+                case 3:
+                    // Sobel Filtresi
+                    processingImage = SobelFilter(originalImage);
+                    break;
+                case 4:
+                    // Alçak Geçiren Filtre
+                    processingImage = LowPassFilter(originalImage);
+                    break;
+                case 5:
+                    // Yüksek Geçiren Filtre
+                    processingImage = HighPassFilter(originalImage);
+                    break;
+                case 6:
+                    // Prewitt Filtresi
+                    processingImage = PrewittFilter(originalImage);
+                    break;
+                case 7:
+                    // Minimum Filtre
+                    processingImage = MinFilter(originalImage);
+                    break;
+                case 8:
+                    // Maximum Filtre
+                    processingImage = MaxFilter(originalImage);
+                    break;
+                case 9:
+                    // Median Filtre
+                    processingImage = MedianFilter(originalImage);
+                    break;
+                case 10:
+                    // K-en Yakın Komşu Filtre
+                    processingImage = KNearestFilter(originalImage, 5);
+                    break;
+                default:
+                    MessageBox.Show("Geçerli bir filtre seçin.");
+                    return;
+            }
 
-            processingImage = YuksekGecirenFiltre(originalImage);
             pictureBox2.Image = processingImage;
+
+
         }
 
         private void clr_Button_Click(object sender, EventArgs e)
@@ -117,12 +437,6 @@ namespace ImageProcessingWork
             originalImage = null;
             processingImage = null;
         }
-
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
 
     }
 }
